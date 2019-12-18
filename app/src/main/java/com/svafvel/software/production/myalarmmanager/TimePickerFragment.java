a1d8a97aebd522573f81e4e7609e588fb4eaeb32
root <root@localhost.localdomain>
2019-12-18 19:00:26 +0700

    Latihan Alarm Manager Android Sederhana

---
+++ app/src/main/java/com/svafvel/software/production/myalarmmanager/TimePickerFragment.java
@@ -0,0 +1,61 @@
+package com.svafvel.software.production.myalarmmanager;
+
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+
+    DialogTimeListener mListener;
+
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+        if(context != null){
+            mListener = (DialogTimeListener) context;
+        }
+    }
+
+    @Override
+    public void onDetach(){
+        super.onDetach();
+        if(mListener != null){
+            mListener = null;
+        }
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+
+        final Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        boolean formatHour24 = true;
+        return new TimePickerDialog(getActivity(), this, hour, minute, formatHour24);
+
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        mListener.onDialogTimeSet(getTag(), hourOfDay, minute);
+    }
+
+    public interface DialogTimeListener{
+        void onDialogTimeSet(String tag, int hourOfDay, int minute);
+    }
+}
